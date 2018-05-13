c900a1bbb5b30a74aea3cc33272b725631b95b4f
Kohei Suenaga <ksuenaga@suenagakouheinoMacBook-Pro.local>
2018-05-13 11:04:46 +0900

    Added filtered_oscillator example.

--- src/hpdr.ml
+++ src/hpdr.ml
@@ -1,9 +1,49 @@
 
 open Core
 
-(* let rec fact n = if n = 1 then 1 else n * fact (n - 1)
- *                
- * let%test _ = fact 5 = 120 *)
-           
+(**************** Commandline args ****************)
+let input_stream = ref stdin
+   
+(**************** Commandline args ****************)
+   
+(* Parse command-line arguments *)
+let parse_commandline_arg () =
+  Arg.parse
+    []
+    (fun s ->
+      try
+        input_stream := open_in s
+      with
+        Not_found ->
+        failwith ("File" ^ s ^ " not found"))
+    ""
+
+(* Parse the input into an S-expr *)
+let parse_input_to_sexpr () =
+  assert false
+
+(* Convert the S-expr to a datatype for verification task *)
+let convert_to_verification_task_from_sexpr sexpr =
+  assert false
+
+(* Verifier core *)
+let verify verifTask =
+  assert false
+
+(* Output the result *)
+let printResult result =
+  assert false
+  
 let _ =
+  let open Mock in
+  (* Parse command-line arguments *)
+  let _ = parse_commandline_arg () in
+  (* Parse the input into an S-expr *)
+  let sexpr = parse_input_to_sexpr () in
+  (* Convert the S-expr to a datatype for verification task *)
+  let verifTask = convert_to_verification_task_from_sexpr sexpr in
+  (* Pass the model to the verifier core routine *)
+  let result = verify verifTask in
+  (* Output the result *)
+  let _ = printResult result in
   ()
