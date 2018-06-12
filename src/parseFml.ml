212cd9b638a7a0cb2a1a7f9ccf9d91286ff29285
Kohei Suenaga <ksuenaga@suenagakouheinoMacBook-Pro.local>
2018-06-12 14:36:08 +0900

    Parser and Z3 interface.

--- src/parseFml.ml
+++ src/parseFml.ml
@@ -7,108 +7,133 @@ open Regex
 open Tokens
 
 type unop = Neg
+[@@deriving show]
+
 type binop = Mul | Div | Add | Sub | Gt | Ge | Lt | Le | Eq | And | Or
+[@@deriving show]                                                                  
                                                                   
 type expr =
   | Binop of binop * expr * expr
   | Unop of unop * expr
   | Ident of string
   | Float of float
+[@@deriving show]           
 
 let infix assoc p op =
-  Infix (p |>> (fun _ a b -> (Binop (op, a, b))), assoc (*Assoc_left*))
+  Infix (p |>> (fun _ a b -> (Binop (op, a, b))), assoc)
 let prefix p op =
   Prefix (p |>> (fun _ a -> (Unop (op, a))))
 
 let operators =
   [
-    [ prefix (string "-") Neg ];
+    [ prefix (symbol "-") Neg ];
     [
-      infix Assoc_left (string "*") Mul;
-      infix Assoc_left (string "/") Div;
+      infix Assoc_left (symbol "*") Mul;
+      infix Assoc_left (symbol "/") Div;
     ];
     [
-      infix Assoc_left (string "+") Add;
-      infix Assoc_left (string "-") Sub;
+      infix Assoc_left (symbol "+") Add;
+      infix Assoc_left (symbol "-") Sub;
     ];
     [
-      infix Assoc_left (string ">") Gt;
-      infix Assoc_left (string ">=")Ge;
-      infix Assoc_left (string "<")  Lt;
-      infix Assoc_left (string "<=") Le;
-      infix Assoc_left (string "==") Eq;
+      infix Assoc_left (symbol ">") Gt;
+      infix Assoc_left (symbol ">=") Ge;
+      infix Assoc_left (symbol "<")  Lt;
+      infix Assoc_left (symbol "<=") Le;
+      infix Assoc_left (symbol "==") Eq;
     ];
     [
-      infix Assoc_left (string "&&") And;
-      infix Assoc_left (string "||") Or;
+      infix Assoc_left (symbol "&") And;
+      infix Assoc_left (symbol "|") Or;
     ];
   ]
 
-let basic =
-  (regexp (make_regexp "[a-zA-Z][a-zA-Z0-9_]*") |>> (fun x -> Ident x)) <|>
-    (integer |>> (fun i -> Float (float_of_int i))) <|>
-    (float |>> (fun f -> Float f))
-  
-let rec expr s =
-  expression operators term s
-and term s =
-  (parens expr <|> basic) s
+let parse_ident = regexp (make_regexp "[a-zA-Z][a-zA-Z0-9_]*") |>> (fun x -> Ident x)
+let parse_float = float |>> (fun f -> Float f)
+let parse_integer = integer |>> (fun i -> Float (float_of_int i))
 
-let rec expr_to_sexp res =
-  let open Sexp in
-  let float f = List [Atom "float"; Atom (string_of_float f)] in
-  let ident x = List [Atom "ident"; Atom x] in
-  let neg a = List [Atom "neg"; a] in
-  let binop s a1 a2 = List [Atom s; a1; a2] in
+let rec basic =
+  spaces >> (parse_ident <|> parse_float <|> parse_integer) << spaces
+
+(* [XXX] Support of parenthesis is buggy. *)
+let rec expr s =
+  ((expression operators (basic <|> parens expr)) << eof) s
+
+  (*
+let rec expr s =
+  ((expression operators (basic <|> term)) << eof) s
+and term s = (parens expr) s
+   *)
+
+
+let rec expr_to_z3 res : Z3.Expr.expr =
+  let open Z3Intf in
+  let float f = mk_real_numeral_s (string_of_float f) in
+  (* [XXX] Consider the type of x *)
+  let ident x = mk_real_var x in
+  let neg a = mk_neg a in
+  (* let binop f a1 a2 = mk_binop f a1 a2 in *)
   match res with
   (* | `Int i -> float i *)
   | Unop(op, a) ->
      begin
        match op with
-       | Neg ->  neg (expr_to_sexp a)
+       | Neg ->  neg (expr_to_z3 a)
      end
   | Binop (op, a, b) ->
-     let op_s = 
+     let op_f = 
        match op with
-       | Add -> "add"
-       | Sub -> "sub"
-       | Mul -> "mul"
-       | Div -> "div"
-       | Gt -> "gt"
-       | Ge -> "ge"
-       | Lt -> "lt"
-       | Le -> "le"
-       | Eq -> "eq"
-       | And -> "and"
-       | Or -> "or"
+       | Add -> mk_add
+       | Sub -> mk_sub
+       | Mul -> mk_mul
+       | Div -> mk_div
+       | Gt -> mk_gt
+       | Ge -> mk_ge
+       | Lt -> mk_lt
+       | Le -> mk_le
+       | Eq -> mk_eq
+       | And -> mk_and
+       | Or -> mk_or
      in
-     binop op_s (expr_to_sexp a) (expr_to_sexp b)
+     op_f (expr_to_z3 a) (expr_to_z3 b)
   | Float f -> float f
   | Ident x ->  ident x
 
 let parse s =
   match MParser.parse_string expr s () with
   | Success e ->
-     expr_to_sexp e
+     expr_to_z3 e
+  (* e *)
   | Failed (msg, e) ->
      failwith msg
 
 let%test_module _ =
   (module struct
-     module S = Sexp
+     open Z3Intf
+     
      let%test _ =
-       let s = parse "4*4+10/2" in
-       let _ = printf "ans:%a@." S.pp s in
-       S.equal (parse "4*4+10/2") (List [Atom "int"; Atom "21.0"])
+       let s = parse " x " in
+       expr_equal s (mk_real_var "x")
+
      let%test _ =
-       S.equal (parse "x>=0") (S.of_string "(>= x 0)")
+       let s = parse "x >= y" in
+       expr_equal s (mk_ge (mk_real_var "x") (mk_real_var "y"))
+       
      let%test _ =
-       S.equal (parse "x >= 0") (S.of_string "(>= x 0)")
-     let%test _ =
-       S.equal (parse "x > 0") (S.of_string "(> x 0)")
-     let%test _ =
-       S.equal (parse "x > 1.0") (S.of_string "(> x 1.0)")
-     let%test _ =
-       S.equal (parse "x <= 0.05") (Sexplib__Sexp.of_string "(<= x 0.05)")
+       let s = parse "4*x+10*x >= y & z >= 5" in
+       expr_equal s 
+         (mk_and
+            (mk_ge
+               (mk_add
+                  (mk_mul
+                     (mk_real_numeral_s "4.0")
+                     (mk_real_var "x"))
+                  (mk_mul
+                     (mk_real_numeral_s "10.0")
+                     (mk_real_var "x")))
+               (mk_real_var "y"))
+            (mk_ge
+               (mk_real_var "z")
+               (mk_real_numeral_s "5.0")))
    end)
     
--- src/z3Intf.ml
+++ src/z3Intf.ml
@@ -32,15 +32,127 @@ let callZ3 z3expr =
 let parse_smtlib2_expr s =
   let open Z3.SMT in
   parse_smtlib2_string !ctx s [] [] [] []
+
+let simplify e =
+  let module Expr = Z3.Expr in
+  Expr.simplify e None
+
+let expr_equal e1 e2 = Z3.Expr.equal e1 e2
   
-let make_expr sexp =
-  E.raise (E.of_string "make_expr: not implemented.")
+let mk_real_var s =
+  let module Sym = Z3.Symbol in
+  let module R = Z3.Arithmetic.Real in
+  R.mk_const !ctx (Sym.mk_string !ctx s)
+
+let%test _ =
+  let module Expr = Z3.Expr in
+  let module R = Z3.Arithmetic.Real in
+  Expr.equal (mk_real_var "x") (R.mk_const_s !ctx "x")
+
+let mk_real_numeral_s s =
+  let module R = Z3.Arithmetic.Real in
+  R.mk_numeral_s !ctx s
+
+let%test _ =
+  let module Expr = Z3.Expr in
+  let module R = Z3.Arithmetic.Real in
+  Expr.equal (mk_real_numeral_s "3.4") (R.mk_numeral_nd !ctx 34 10)
+
+let mk_neg e =
+  let module A = Z3.Arithmetic in
+  let module R = A.Real in
+  A.mk_mul !ctx [e; R.mk_numeral_nd !ctx (-1) 1]
+
+let%test _ =
+  let module Expr = Z3.Expr in
+  let module R = Z3.Arithmetic.Real in
+  Expr.equal (simplify (mk_neg (mk_real_numeral_s "3"))) (mk_real_numeral_s "-3")
+
+let mk_true = Z3.Boolean.mk_true !ctx
+let mk_false = Z3.Boolean.mk_false !ctx
+            
+let mk_add e1 e2 = Z3.Arithmetic.mk_add !ctx [e1; e2]
+let mk_sub e1 e2 = Z3.Arithmetic.mk_sub !ctx [e1; e2]
+let mk_mul e1 e2 = Z3.Arithmetic.mk_mul !ctx [e1; e2]
+let mk_div e1 e2 = Z3.Arithmetic.mk_div !ctx e1 e2
+let mk_gt e1 e2 = Z3.Arithmetic.mk_gt !ctx e1 e2
+let mk_ge e1 e2 = Z3.Arithmetic.mk_ge !ctx e1 e2
+let mk_lt e1 e2 = Z3.Arithmetic.mk_lt !ctx e1 e2
+let mk_le e1 e2 = Z3.Arithmetic.mk_le !ctx e1 e2
+let mk_eq e1 e2 = Z3.Boolean.mk_eq !ctx e1 e2
+let mk_and e1 e2 = Z3.Boolean.mk_and !ctx [e1; e2]
+let mk_or e1 e2 = Z3.Boolean.mk_or !ctx [e1; e2]
   
 let%test_module _ =
   (module struct
-     open Sexp
-     open Z3.Arithmetic.Real
-     open Z3.Expr
-     let _ = equal (make_expr (of_string "(int 3)")) (mk_numeral_i !ctx 3)
-   end)
+     module Expr = Z3.Expr
+     module R = Z3.Arithmetic.Real
+     module B = Z3.Boolean
+
+     let%test _ =
+       Expr.equal
+         (simplify (mk_add (mk_real_numeral_s "3") (mk_real_numeral_s "4")))
+         (mk_real_numeral_s "7")
+       
+     let%test _ =
+       Expr.equal
+         (simplify (mk_add (mk_real_numeral_s "3") (mk_real_numeral_s "4")))
+         (mk_real_numeral_s "7")
+
+     let%test _ =
+       Expr.equal
+         (simplify (mk_mul (mk_real_numeral_s "3") (mk_real_numeral_s "4")))
+         (mk_real_numeral_s "12")
+
+     let%test _ =
+       Expr.equal
+         (simplify (mk_div (mk_real_numeral_s "3") (mk_real_numeral_s "4")))
+         (mk_real_numeral_s "3/4")
+
+     let%test _ =
+       Expr.equal
+         (simplify (mk_gt (mk_real_numeral_s "3") (mk_real_numeral_s "4")))
+         mk_false
+
+     let%test _ =
+       Expr.equal
+         (simplify (mk_ge (mk_real_numeral_s "4") (mk_real_numeral_s "4")))
+         mk_true
+
+     let%test _ =
+       Expr.equal
+         (simplify (mk_lt (mk_real_numeral_s "3") (mk_real_numeral_s "4")))
+         mk_true
+       
+     let%test _ =
+       Expr.equal
+         (simplify (mk_le (mk_real_numeral_s "3") (mk_real_numeral_s "3")))
+         mk_true
+       
+     let%test _ =
+       Expr.equal
+         (simplify (mk_eq (mk_real_numeral_s "3") (mk_real_numeral_s "4")))
+         mk_false
+
+     let%test _ =
+       Expr.equal
+         (simplify (mk_and mk_true mk_false))
+         mk_false
+
+     let%test _ =
+       Expr.equal
+         (simplify (mk_or mk_true mk_false))
+         mk_true
+       
+end)
+
+(* let make_expr sexp =
+ *   E.raise (E.of_string "make_expr: not implemented.") *)
   
+(* let%test_module _ =
+ *   (module struct
+ *      open Sexp
+ *      open Z3.Arithmetic.Real
+ *      open Z3.Expr
+ *      let _ = equal (make_expr (of_string "(int 3)")) (mk_numeral_i !ctx 3)
+ *    end) *)
