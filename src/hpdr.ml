13d3688baa743eae0671f9b5bfbe162dce4bdeda
Kohei Suenaga <ksuenaga@suenagakouheinoMacBook-Pro.local>
2018-09-18 12:27:46 +0900

    Added tests.

--- src/hpdr.ml
+++ src/hpdr.ml
@@ -76,7 +76,17 @@ let%test _ =
   true
 *)
 
-(*
+let%test _ =
+  let open Z3Intf in
+  let open Cnf in
+  let open ParseFml in
+  let models = SpaceexComponent.parse_from_channel (In_channel.create (!Config.srcroot ^ "/examples/examples/line2/line.xml")) in
+  let model = List.hd_exn models in
+  let res = verify ~init_id:(SpaceexComponent.id_of_string "1") ~model:model ~init:(parse_to_cnf "x <= 0") (* Cnf.cnf_true *) ~safe:(parse_to_cnf "x < 1.0") in
+  let _ = printResult res in
+  true
+  
+  (*
 let%test _ =
   let open Z3Intf in
   let open Cnf in
@@ -86,8 +96,9 @@ let%test _ =
   let res = verify ~init_id:(SpaceexComponent.id_of_string "1") ~model:model ~init:(parse_to_cnf "x == 0.5") (* Cnf.cnf_true *) ~safe:(parse_to_cnf "x <= 1.0") in
   let _ = printResult res in
   true
-  *)
+   *)
 
+(*
 let%test _ =
   let open Z3Intf in
   let open Cnf in
@@ -97,6 +108,7 @@ let%test _ =
   let res = verify ~init_id:(SpaceexComponent.id_of_string "1") ~model:model ~init:(parse_to_cnf "x == 0.5 & y == 0.0") (* Cnf.cnf_true *) ~safe:(parse_to_cnf "x <= 1.0") in
   let _ = printResult res in
   true
+   *)
 
 (*
 let%test _ =
